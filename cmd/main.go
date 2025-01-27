a7b4c8f9e7506a0060a3df5e9928916e93fd2a5f
20fut <231612@astanait.edu.kz>
2025-01-27 16:11:46 +0500

    check for connection to database(simple ex)

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"Marketplace-cs2-/database"
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func main() {
-	fmt.Println("CS2 Skins Marketplace Server Running...")
+	database.ConnectDB()
+
+	usersCollection := database.GetCollection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var result bson.M
+	err := usersCollection.FindOne(ctx, bson.M{"steam_id": "test_steam_id"}).Decode(&result)
+	if err != nil {
+		log.Fatalf("Error fetching user: %v", err)
+	}
+
+	fmt.Printf("Fetched user: %v\n", result)
 }
