77f2e91f573c7b125df5c75e93f31da1b58cfac6
20fut <231612@astanait.edu.kz>
2025-01-27 15:44:58 +0500

    poor db connection in database, and updated mongo god

--- database/mongo.go
+++ database/mongo.go
@@ -1,1 +1,39 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+// ConnectDB initializes the MongoDB connection
+func ConnectDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	// Ping the database to ensure connection is established
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("MongoDB connection ping failed: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	Client = client
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return Client.Database("cs2_skins_marketplace").Collection(collectionName)
+}
