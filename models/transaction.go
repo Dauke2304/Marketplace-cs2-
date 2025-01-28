6eac365ffcad055640540986c5822bfdefacbc8c
20fut <231612@astanait.edu.kz>
2025-01-29 02:20:07 +0500

    fields for transaction.go

--- models/transaction.go
+++ models/transaction.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Transaction struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	BuyerID  primitive.ObjectID `bson:"buyer_id"`
+	SellerID primitive.ObjectID `bson:"seller_id"`
+	SkinID   primitive.ObjectID `bson:"skin_id"`
+	Amount   float64            `bson:"amount"`
+	Date     time.Time          `bson:"date"`
+}
