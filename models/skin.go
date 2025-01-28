f74bd1ad6542686c34a6f2d254bf7f780c8e8e38
20fut <231612@astanait.edu.kz>
2025-01-29 02:19:30 +0500

    fields for skin.go

--- models/skin.go
+++ models/skin.go
@@ -1,1 +1,11 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Skin struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	Name    string             `bson:"name"`
+	Price   float64            `bson:"price"`
+	Image   string             `bson:"image"`
+	OwnerID primitive.ObjectID `bson:"owner_id,omitempty"` // null if available for sale
+}
