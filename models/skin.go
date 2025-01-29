5309c6c9fcd05db5700743188bde2437ea369dd6
20fut <231612@astanait.edu.kz>
2025-01-29 15:15:09 +0500

    updated skin.go model

--- models/skin.go
+++ models/skin.go
@@ -3,9 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Skin struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	Name    string             `bson:"name"`
-	Price   float64            `bson:"price"`
-	Image   string             `bson:"image"`
-	OwnerID primitive.ObjectID `bson:"owner_id,omitempty"` // null if available for sale
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	Description string             `bson:"description" json:"description"`
+	Price       float64            `bson:"price" json:"price"`
+	Image       string             `bson:"image" json:"image"`
+	Rarity      string             `bson:"rarity" json:"rarity"`               // e.g., Common, Rare, Epic, Legendary
+	Condition   string             `bson:"condition" json:"condition"`         // e.g., Factory New, Minimal Wear, etc.
+	OwnerID     primitive.ObjectID `bson:"owner_id,omitempty" json:"owner_id"` // null if available for sale
+	IsListed    bool               `bson:"is_listed" json:"is_listed"`         // true if the skin is listed for sale
 }
