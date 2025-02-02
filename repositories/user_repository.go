54524e2d4e11bd97460e86cb9aa830bbdef52648
20fut <231612@astanait.edu.kz>
2025-02-02 22:33:58 +0500

    User_repo full

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,6 +1,11 @@
 package repositories
 
 import (
+	"Marketplace-cs2-/models"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,4 +17,69 @@ func NewUserRepository(db *mongo.Database) *UserRepository {
 	return &UserRepository{
 		collection: db.Collection("users"),
 	}
+}
+
+// CreateUser inserts a new user into the database
+func (r *UserRepository) CreateUser(user models.User) (primitive.ObjectID, error) {
+	result, err := r.collection.InsertOne(context.Background(), user)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+	return result.InsertedID.(primitive.ObjectID), nil
+}
+
+func (r *UserRepository) GetUserByID(userID primitive.ObjectID) (*models.User, error) {
+	var user models.User
+	filter := bson.M{"_id": userID}
+	err := r.collection.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	filter := bson.M{"email": email}
+	err := r.collection.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) GetUserBySteamID(steamID string) (*models.User, error) {
+	var user models.User
+	filter := bson.M{"steam_id": steamID}
+	err := r.collection.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) UpdateUser(userID primitive.ObjectID, update bson.M) error {
+	filter := bson.M{"_id": userID}
+	_, err := r.collection.UpdateOne(context.Background(), filter, bson.M{"$set": update})
+	return err
+}
+
+func (r *UserRepository) DeleteUser(userID primitive.ObjectID) error {
+	filter := bson.M{"_id": userID}
+	_, err := r.collection.DeleteOne(context.Background(), filter)
+	return err
+}
+
+func (r *UserRepository) GetAllUsers() ([]models.User, error) {
+	cursor, err := r.collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+
+	var users []models.User
+	if err := cursor.All(context.Background(), &users); err != nil {
+		return nil, err
+	}
+	return users, nil
 }
