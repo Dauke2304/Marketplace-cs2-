4e477ef66f68e98acf45ef9e374241bc1bbdfd50
20fut <231612@astanait.edu.kz>
2025-02-18 20:23:47 +0500

    last 3

--- setup/setup.go
+++ setup/setup.go
@@ -12,11 +12,11 @@ func SeedAdmin() {
 	db := database.Client.Database("cs2_skins_marketplace")
 	userRepo := repositories.NewUserRepository(db)
 
-	adminUser, _ := userRepo.GetUserByUsername("admin")
+	adminUser, _ := userRepo.GetUserByUsername("admin123")
 	if adminUser == nil {
 		hashedPass, _ := bcrypt.GenerateFromPassword([]byte("admin123"), bcrypt.DefaultCost)
 		userRepo.CreateUser(models.User{
-			Username: "admin",
+			Username: "admin123",
 			Password: string(hashedPass),
 			IsAdmin:  true,
 		})
