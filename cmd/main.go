0d687122b449fe9f29cad068a11a9de0cf9a2821
20fut <231612@astanait.edu.kz>
2025-02-18 19:54:28 +0500

    last 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"Marketplace-cs2-/database"
 	"Marketplace-cs2-/routes"
+	"Marketplace-cs2-/setup"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	if err := database.InitDB(); err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	setup.SeedAdmin()
 	routes.InitRoutes()
 	log.Fatal(http.ListenAndServe(":9000", nil))
 	fmt.Println("Server running at :9000")
---
+++ services/admin.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"Marketplace-cs2-/database"
+	"Marketplace-cs2-/models"
+	"Marketplace-cs2-/repositories"
+	"net/http"
+	"text/template"
+)
+
+func HandleAdminPanel(w http.ResponseWriter, r *http.Request) {
+	db := database.Client.Database("cs2_skins_marketplace")
+
+	userRepo := repositories.NewUserRepository(db)
+	skinRepo := repositories.NewSkinRepository(db)
+	transactionRepo := repositories.NewTransactionRepository(db)
+
+	users, _ := userRepo.GetAllUsers()
+	skins, _ := skinRepo.GetAllSkins()
+	transactions, _ := transactionRepo.GetAllTransactions()
+
+	data := struct {
+		Users        []models.User
+		Skins        []models.Skin
+		Transactions []models.Transaction
+	}{
+		Users:        users,
+		Skins:        skins,
+		Transactions: transactions,
+	}
+
+	tmpl := template.Must(template.ParseFiles("../frontend/templates/admin.html"))
+	tmpl.Execute(w, data)
+}
+func GetCurrentUser(r *http.Request) *models.User {
+	// Get session token from cookies
+	cookie, err := r.Cookie("sessiontoken")
+	if err != nil {
+		return nil
+	}
+
+	// Initialize database and repository
+	db := database.Client.Database("cs2_skins_marketplace")
+	userRepo := repositories.NewUserRepository(db)
+
+	// Get user by session token
+	user, err := userRepo.GetUserBySessionToken(cookie.Value)
+	if err != nil {
+		return nil
+	}
+
+	return user
+}
